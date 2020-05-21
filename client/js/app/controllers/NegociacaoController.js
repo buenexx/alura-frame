37801c6281f1a3053004183fe625d8916e37b187
Fernando Bueno <buenofernando2013@gmail.com>
2020-05-21 18:32:50 -0300

    commit

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,51 +3,58 @@ class NegociacaoController {
     constructor() {
         
         let $ = document.querySelector.bind(document);
+        
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
-        let self = this;
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-            get(target, prop, receiver){
-                if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)){
-                    return function(){
-                        console.log(`interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return Reflect.get(target, prop, receiver);
-            }
-        });
-
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-        
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
-        
+         
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(), 
+            new NegociacoesView($('#negociacoesView')), 
+            'adiciona', 'esvazia');
+       
+        this._mensagem = new Bind(
+            new Mensagem(), new MensagemView($('#mensagemView')),
+            'texto');       
     }
     
     adiciona(event) {
         
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
-        
+        this._mensagem.texto = 'Negociação adicionada com sucesso'; 
         this._limpaFormulario();   
+    }
+
+    importaNegociacoes() {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/semana');
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4){
+                if (xhr.status == 200) {
+                    console.log('obtendo as negociações do servidor')
+                    console.log(xhr.responseText);
+                    JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                        .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                } else {
+                    console.log('Não foi possivel obter as negociações do servidor');
+                    console.log(xhr.responseText);
+                }
+            }
+        };
+
+        xhr.send();
     }
     
     apaga() {
         
         this._listaNegociacoes.esvazia();
-         
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
+    
     _criaNegociacao() {
         
         return new Negociacao(
--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -1,16 +1,36 @@
 class ProxyFactory {
-    static createProxy(objeto, props, acao){
-        return new Proxy(new ListaNegociacoes(), {
-            get(target, prop, receiver){
-                if(props.includes(prop) && typeof(target[prop]) == typeof(Function)){
-                    return function(){
-                        console.log(`interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);
-                        acao(target);
+    
+    static create(objeto, props, acao) {
+     
+        return new Proxy(objeto, {
+                
+                get(target, prop, receiver) {
+                    
+                    if(props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
+                        
+                        return function() {
+                            
+                            console.log(`interceptando ${prop}`);
+                            let retorno = Reflect.apply(target[prop], target, arguments);
+                            acao(target);
+                            return retorno;
+                        }
                     }
+                    
+                    return Reflect.get(target, prop, receiver);
+                },
+                
+                set(target, prop, value, receiver) {
+                    
+                    let retorno = Reflect.set(target, prop, value, receiver);
+                    if(props.includes(prop)) acao(target);
+                    return retorno;
                 }
-                return Reflect.get(target, prop, receiver);
-            }
         });
+    }
+    
+    static _ehFuncao(func) {
+        
+        return typeof(func) == typeof(Function);
     }
 }
